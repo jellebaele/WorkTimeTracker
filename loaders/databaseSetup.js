2ed469a6643cbf02350d4abbd3d29e351fc59b83
Jelle Baele <jellebaele@hotmail.com>
2022-01-17 21:42:27 +0100

    Test commit

--- loaders/databaseSetup.js
+++ loaders/databaseSetup.js
@@ -4,6 +4,6 @@ const keys = require('../config/keys');
 module.exports = () => {
    mongoose.connect(keys.mongoUri, (error) => {
       if (error) console.error('Failed to connect to database. ', error);
-      else console.log('Connected to db.');
+      else console.log('Connected to database.');
    });
 };
